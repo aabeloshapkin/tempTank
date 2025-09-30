36a8f0cbc4d916ef45b63f951028f80d4610721c
Анатолий Белошапкин <aabeloshapkin@gmail.com>
2025-09-30 21:21:36 +0900

    `Added rotation functionality to player.js and updated style.css to hide overflow.`

--- js/player.js
+++ js/player.js
@@ -3,31 +3,39 @@ export default class Player {
         this.x = x;
         this.y = y;
         this.context = context;
-        this.cursorPosition = {
-            x: 0,
-            y: 0
-        }
 
-        document.addEventListener("mousemove", event => {
-            this.cursorPosition.x =event.clientX;
-            this.cursorPosition.y =event.clientY;
-        });
+        this.rotationAngle = 0; // Current rotation angle in radians
+        this.rotationSpeed = 0.01; // Rotation speed per frame
+        this.rotationDirection = 0; // -1 for left, 1 for right, 0 for no rotation
 
         this.image = new Image();
         this.image.src = "./img/tank.png";
         this.imageWidth = 40;
         this.imageHeight = 50;
+
+        // Keyboard event listeners for rotation control
+        document.addEventListener("keydown", event => {
+            if (event.code === "ArrowLeft") {
+                this.rotationDirection = -1;
+            } else if (event.code === "ArrowRight") {
+                this.rotationDirection = 1;
+            }
+        });
+
+        document.addEventListener("keyup", event => {
+            if (event.code === "ArrowLeft" && this.rotationDirection === -1) {
+                this.rotationDirection = 0;
+            } else if (event.code === "ArrowRight" && this.rotationDirection === 1) {
+                this.rotationDirection = 0;
+            }
+        });
     }
 
     drawImg() {
         this.context.drawImage(
             this.image,
-            // 0,
-            // 0,
-            // this.imageWidth,
-            // this.imageHeight,
-            this.x - this.imageWidth/2,
-            this.y - this.imageHeight/2,
+            this.x - this.imageWidth / 2,
+            this.y - this.imageHeight / 2,
             this.imageWidth,
             this.imageHeight
         );
@@ -35,10 +43,14 @@ export default class Player {
 
     draw() {
         this.context.save();
-        let angle = Math.atan2(this.cursorPosition.y - this.y, this.cursorPosition.x - this.x);
+
+        // Update rotation angle based on rotation direction and speed
+        this.rotationAngle += this.rotationDirection * this.rotationSpeed;
+
         this.context.translate(this.x, this.y);
-        this.context.rotate(angle + Math.PI/2);
-        this.context.translate(- this.x, -this.y);
+        this.context.rotate(this.rotationAngle);
+        this.context.translate(-this.x, -this.y);
+
         this.drawImg();
         this.context.restore();
     }
